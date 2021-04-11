395051fc258f8cb20084548ad5892e4a840d8cda
X-ANS <a.aitelaouad@enim.ac.ma>
2021-04-11 23:32:52 +0000

    Fix Inheritence

--- account_activity_prestation_14/__manifest__.py
+++ account_activity_prestation_14/__manifest__.py
@@ -25,7 +25,7 @@
         'data/prestation_sequence.xml',
         'views/account.xml',
         'views/activity_prestation.xml',
-        # 'views/processus.xml',
+        'views/processus.xml',
         # 'views/analytic_key.xml',
         # 'views/attribut.xml',
         ],
